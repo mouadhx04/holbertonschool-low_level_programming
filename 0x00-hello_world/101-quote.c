62a743e304b456ba04875e3ef3559bc9e0d60448
mouadhx04 <mouadh1500@gmail.com>
2021-06-17 10:00:26 -0700

    not from the library

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-write("\and that piece of art is useful" - Dora Korpar, 2015-10-19");
+write("\and that piece of art is useful" - Dora Korpar, 2015-10-19"");
 return (1);
 }
