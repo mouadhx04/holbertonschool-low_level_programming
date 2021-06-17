ee68dc0d0d7962693de197cb13ed5f94366a0ed5
mouadhx04 <mouadh1500@gmail.com>
2021-06-17 06:20:34 -0700

    print size of on byte

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,16 +6,10 @@
  */
 int main(void)
 {
-char charType;
-int intType;
-double doubleType;
-long double doublexType;
-float floatType;
-
-printf("size of a char:%zu byte\n", sizeof(charType));
-printf("Size of a int:%zu byte\n", sizeof(intType));
-printf("size of a long int:%zu byte\n", sizeof(doubleType));
-printf("size of a long long int:%zu byte\n", sizeof(doublexType));
-printf("size of a float:%zu byte\n", sizeof(floatType));
+printf("size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of a int: %zu  byte(s)\n", sizeof(int));
+printf("size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("size of a float: %zu byte(s)\n", sizeof(float));
 return (0);
 }
