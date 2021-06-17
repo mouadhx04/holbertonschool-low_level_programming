750ed38ca0314becc0ff0c622aa42c145a8cd3ae
mouadhx04 <mouadh1500@gmail.com>
2021-06-17 06:03:35 -0700

    print with sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,13 +9,14 @@ int main(void)
 char charType;
 int intType;
 float floatType;
-double double doublex2Type;
 double doubleType;
+double double doublexType;
 float floatType;
+
 printf("size of a char:", sizeof(charType));
 printf("Size of a int:", sizeof(intType));
-printf("size of a long long int:", sizeof(doublex2Type));
 printf("size of a long int:", sizeof(doubleType));
+printf("size of a long long int:", sizeof(doublexType));
 printf("size of a float:", sizeof(floatType));
 return (0);
 }
