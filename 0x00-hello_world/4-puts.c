9f235463c644c5bba768e845ee75ebd564f5004d
mouadhx04 <mouadh1500@gmail.com>
2021-06-17 05:13:08 -0700

    first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
-include <stdio.h>
-//*puts replace printf
-//
-int main (void)
+#include<stdio.h>
+/**
+*puts replace printf
+*/
+int main(void)
 {
-puts("Programming is like building a multilingual puzzle \n");
+puts("\"Programming is like building a multilingual puzzle");
 return(0);
 }
