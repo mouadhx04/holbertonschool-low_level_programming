b08a81e7c6976f108417f26e529aa366ff7ca04f
mouadhx04 <mouadh1500@gmail.com>
2021-06-18 09:13:37 -0700

    nb base 16 with putchar

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,10 +9,11 @@
 int main(void)
 {
 char i, j;
-for (i = 0; i < 10; i++)
+for (i = 48; i <= 57; i++)
 {
 if (j = 'A' && j <='F')
-putchar(i,j);
+putchar(i);
+putchar(j);
 }
 putchar('\n');
 return (0);
