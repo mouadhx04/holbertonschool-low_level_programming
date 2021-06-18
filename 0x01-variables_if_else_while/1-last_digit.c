c068358237396ac46f56e01c9fe98a16ba933f0b
mouadhx04 <mouadh1500@gmail.com>
2021-06-18 05:03:53 -0700

    if statement with other case

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 5)
+printf("%c is and is greater than 5\n", n);
+else if (n == 0)
+printf("%c is and is 0\n", n);
+else if (n < 6 && n != 0)
+printf("%c and is less than 6 and not 0\n", n);
+return (0);
+}
