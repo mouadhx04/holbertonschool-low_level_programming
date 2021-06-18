2e232b8cf1a08fea8db84af628ca7b7b68fc56b9
mouadhx04 <mouadh1500@gmail.com>
2021-06-18 10:31:00 -0700

    boucle for putchar

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,18 +8,15 @@
  */
 int main(void)
 {
-int i,
-for (i = 48; i <= 57; i++)
-{
 int i;
-for (i = 48; i <= 57; i++)
+for(i = 48; i <= 57; i++)
 {
 putchar(i);
-}
-if (i > 57)
+if (i < 57)
 {
 putchar(',');
 putchar(' ');
+}
 }
 putchar('\n');
 return (0);
