defbe028b7d03af6df3fe9e0084c9e5a71a2d5f1
mouadhx04 <mouadh1500@gmail.com>
2021-06-18 05:12:52 -0700

    statement if with other case

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,10 +12,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 5)
-printf("%c is and is greater than 5\n", n);
+printf("Last digit of", n " %c and is greater than 5\n");
 else if (n == 0)
-printf("%c is and is 0\n", n);
+printf("Last digit of", n "is %c and is 0\n");
 else if (n < 6 && n != 0)
-printf("%c and is less than 6 and not 0\n", n);
+printf("Last digit of", n "is %c and is less than 6 and not 0\n");
 return (0);
 }
