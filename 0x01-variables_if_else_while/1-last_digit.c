1823ad4704c7c8193ef5143a5c4d1b66cb1baca4
mouadhx04 <mouadh1500@gmail.com>
2021-06-18 05:40:03 -0700

    first fix a ccondition

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,10 +13,10 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 x = n % 10;
 if ( x > 5 )
-printf("Last digit of %c is %x and is greater than 5\n");
+printf("Last digit of %c is %x and is greater than 5\n", n, i);
 else if (x < 6 && x != 0)
-printf("Last digit of %c is %x and is less than 6 and not 0\n");
+printf("Last digit of %c is %x and is less than 6 and not 0\n", n, i);
 else if (x == 0)
-printf("Last digit of %c is %x and is 0\n");
+printf("Last digit of %c is %x and is 0\n", n,i);
 return (0);
 }
