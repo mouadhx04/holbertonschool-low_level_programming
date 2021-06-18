bd15db64d4bcac872c108fd3a0359d20eab3636b
mouadhx04 <mouadh1500@gmail.com>
2021-06-18 06:15:43 -0700

    alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alpha = 'a';
+putchar("Alphabet in lowercase\n");
+while(alpha <= z)
+{
+putchar("%c\n", alpha);
+alpha++;
+}
+return (0);
+}
