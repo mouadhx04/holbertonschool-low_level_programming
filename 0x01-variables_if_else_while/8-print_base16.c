36c9454f1df3c8cf233144a778c4a9fd8b512905
mouadhx04 <mouadh1500@gmail.com>
2021-06-18 09:07:59 -0700

    nb base 16 with putchar

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char i, j;
+for (i = 0; i < 10; i++)
+{
+if (j = 'A' && j <='F')
+putchar(i,j);
+}
+putchar('\n');
+return (0);
+}
