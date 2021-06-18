50791a6ca480dd41d2c426cf0805cf273d191c25
mouadhx04 <mouadh1500@gmail.com>
2021-06-18 06:51:50 -0700

    alpabet in lowercase with while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char i;
+while (i== 'a' && i <= 'z' != 'q' && i != 'e')
+{
+putchar(i);
+}
+putchar('\n');
+return(0);
+}
+
