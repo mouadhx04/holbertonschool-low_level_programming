e90ab9c4fa9a5b62cb53c382509e7a32f9897aa9
mouadhx04 <mouadh1500@gmail.com>
2021-06-18 16:02:49 -0700

    deux boucle with two if

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+int j;
+for (i = 48; i < 57; i++)
+{
+for (j = 49; j <= 57; j++)
+{
+if (j > i)
+{
+putchar(i);
+putchar(j);
+if (i < 56)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
