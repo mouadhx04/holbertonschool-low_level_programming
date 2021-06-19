debb843c6cec420398b1c0ea03596095666aabc7
mouadhx04 <mouadh1500@gmail.com>
2021-06-19 13:29:28 -0700

    more statement for

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+int j;
+int x;
+int s;
+for (i = 48; i < 57; i++)
+{
+for (j = 48; j <= 57; j++)
+{
+for (x = 48; x <= 57; x++)
+{
+for (s = 49; x <= 57; s++)
+{
+if (i > j)
+putchar(i);
+putchar(j);
+putchar(' ');
+putchar(x);
+putchar(s);
+if (j < 56)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
+
+
