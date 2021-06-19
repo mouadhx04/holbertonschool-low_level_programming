311cffc6755c6819b39784b854812e40ee7c39a9
mouadhx04 <mouadh1500@gmail.com>
2021-06-19 06:10:35 -0700

    three boucle for

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+int j;
+int x;
+for (i = 48; i < 57; i++)
+{
+for (j = 49; j <= 57; j++)
+{
+for (x = 50; x <= 57; x++)
+{
+if (j > i && x > j)
+{
+putchar(i);
+putchar(j);
+putchar(x);
+if (i <= 55)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
