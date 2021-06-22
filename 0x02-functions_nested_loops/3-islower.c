626ffccc35ccd550f81110579fd5343a6d45ffe4
mouadhx04 <mouadh1500@gmail.com>
2021-06-22 15:24:19 -0700

    return is the results and take care in the comments

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,13 @@
 
 /**
  * _islower - checks for lowercase character
- *@x: The character to check
+ *@c: The character to check
  *
  * Return: 1 (Success)
  */
 int _islower(int c)
 {
-if (x < 'z' && x > 'a')
+if (c < 'z' && c > 'a')
 return (1);
 else
 return (0);
