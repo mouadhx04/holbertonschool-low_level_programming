9eacae10e2cee254d79b25aea3f6de657e86bf07
mouadhx04 <mouadh1500@gmail.com>
2021-06-22 12:09:30 -0700

    add prototype

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+print_alphabet();
+char i;
+for (i = 97, i < 122, i++)
+{
+_putchar(i);
+_putchar('\n);
+}
+return (0);
+}
