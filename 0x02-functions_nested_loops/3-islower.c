feb1c8790c33716ac335d234447b34a78d3cb38b
mouadhx04 <mouadh1500@gmail.com>
2021-06-22 15:08:26 -0700

    3three comments and call a function

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,15 +7,10 @@
  *
  * Returns 0 otherwise
  */
-int main(void)
+int _islower(int c)
 {
-int r;
-r = _islower('H');
-_putchar(r + '0');
-r = _islower('o');
-_putchar(r + '0');
-r = _islower(108);
-_putchar(r + '0');
-_putchar('\n');
+int c;
+if (c >= 'a' && c <= 'z')
+_islower = 1;
 return (0);
 }
