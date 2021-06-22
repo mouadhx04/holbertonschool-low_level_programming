6fc758f6358873bd3192a2c3561f68c28dd62dc8
mouadhx04 <mouadh1500@gmail.com>
2021-06-22 13:17:07 -0700

    replace void and take care about return

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
- * void print_alphabet(void)
+ * print_alphabet-prints the alphabet, in lowercase
  *
- * prints the alphabet, in lowercase
+ * Return: Always 0 (Success)
  */
 void print_alphabet(void) 
 {
