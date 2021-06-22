5ce9c2bae14d0967507db3b191aa34e7ea0e6859
mouadhx04 <mouadh1500@gmail.com>
2021-06-22 13:48:33 -0700

    replace main() and no return

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,15 @@
 #include "holberton.h"
+
 /**
- * print_alphabet-prints the alphabet, in lowercase
+ * print_alphabet - prints prints the alphabet, in lowercase
  *
  * Return: Always 0.
  */
-void print_alphabet(void) 
+void print_alphabet(void)
 {
-char i;
-for (i = 'a'; i <= 'z'; i++)
-_putchar(i);
-_putchar('\n');
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	_putchar(i);
+	_putchar('\n');
 }
