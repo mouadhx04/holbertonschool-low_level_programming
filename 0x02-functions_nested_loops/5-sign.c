50406385cc24cbe7c1e1ff0172721bded680f93e
mouadhx04 <mouadh1500@gmail.com>
2021-06-22 15:58:19 -0700

    NB comments

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,12 +10,18 @@
 int print_sign(int n)
 {
 if (n > 0)
-return(1);
+{
 _putchar('+');
+return (1);
+}
 else if (n < 0)
-return (-1);
+{
 _putchar('-');
-else 
-return (0);
+return (-1);
+}
+else
+{
 _putchar('0');
+return (0);
+}
 }
