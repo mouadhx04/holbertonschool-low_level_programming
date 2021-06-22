402d53b7d588b9c86828a9ab15294fcce7408d14
mouadhx04 <mouadh1500@gmail.com>
2021-06-22 14:00:38 -0700

    replace main and return 0

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	_putchar(i);
+	_putchar(i);
+	_putchar(i);
+	_putchar(i);
+	_putchar(i);
+	_putchar(i);
+	_putchar(i);
+	_putchar(i);
+	_putchar(i);
+	_putchar(i);
+	_putchar('\n');
+}
