a5fe5b841eb22f0f27d704ad238ac6aea8ca8a2c
mouadhx04 <mouadh1500@gmail.com>
2021-06-22 15:34:02 -0700

    NB comments and returns()

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _isalpha - checks for alphabetic character
+ *@c: The character to check
+ *
+ * Return: 1 (Success)
+ */
+int _isalpha(int c)
+{
+if (c < 'z' && c > 'a')
+	return (1);
+else if (c < 'Z' && c > 'A')
+	return (1);
+else 
+	return (0);
+}
