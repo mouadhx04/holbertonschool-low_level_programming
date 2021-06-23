45f42f5e0d3e84ada38f6f4f2c0e961a0ea69002
mouadhx04 <mouadh1500@gmail.com>
2021-06-23 04:15:52 -0700

    two loops with / and % int _putchar

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * jack_bauer -prints every minute of the day of Jack Bauer
+ *
+ * Return: nothing
+ */
+void jack_bauer(void)
+{
+	int a;
+	int b;
+	for (a = 0; a <= 23; a++) /*hours*/
+	{
+		for (b = 0; b <= 59; b++) /*minutes*/
+		{
+			_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+			_putchar(':');
+			_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+			_putchar ('\n');
+		}
+	}
+}
+
