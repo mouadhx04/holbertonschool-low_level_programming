ba097cf0edaa6cca05460bf7c4f452491d2977de
mouadhx04 <mouadh1500@gmail.com>
2021-06-23 02:14:52 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ *@n: int to check
+ *
+ * Return: n % 10 (Success)
+ */
+int print_last_digit(int n)
+{
+return (n % 10);
+}
