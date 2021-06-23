012f469d3421a66cd113412852b5103886ad9e7f
mouadhx04 <mouadh1500@gmail.com>
2021-06-23 02:45:38 -0700

    putchar and return

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,11 +6,11 @@
  * Return: n % 10 (Success)
  */
 int print_last_digit(int n)
-{	
+{
 if (n < 0)
 {
 n = n * -1;
-_putchar(n % 10 + '0');
+_putchar((n % 10) + '0');
 return (n % 10);
 }
 _putchar(n % 10 + '0');
