e5d003e5e81751e782806282d04e66b6224b0901
mouadhx04 <mouadh1500@gmail.com>
2021-06-24 14:59:46 -0700

    prints square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,13 +12,11 @@ void print_square(int size)
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
-		{
 			_putchar('#');
 			if (i == size - 1)
 				continue;
 			_putchar('\n');
-		}
 	}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
