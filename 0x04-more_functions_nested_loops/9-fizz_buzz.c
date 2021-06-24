6830f73d75c2dc24affa5dd30336eae089403b35
mouadhx04 <mouadh1500@gmail.com>
2021-06-24 15:40:12 -0700

    fizz buzz game

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 /**
- *Fizz-Buzz test
- *
+ *main - Fizz-Buzz test on integer
+ *Return: 0 (success)
  */
 int main(void)
 {
-    int i;
-    for(i=1; i<=100; i++)
-    {
-        if(((i%3)||(i%5))== 0)
-            printf("number= %d FizzBuzz\n", i);
-        else if((i%3)==0)
-            printf("number= %d Fizz\n", i);
-        else if((i%5)==0)
-            printf("number= %d Buzz\n", i);
-        else
-            printf("number= %d\n",i);
+int i;
 
-    }
-
-    return 0;
+for (i = 1; i <= 100; i++)
+{
+if (((i % 3) || (i % 5)) == 0)
+printf("number= %d FizzBuzz\n", i);
+else if ((i % 3) == 0)
+printf("number= %d Fizz\n", i);
+else if ((i % 5) == 0)
+printf("number= %d Buzz\n", i);
+else
+printf("number= %d\n", i);
+}
+return (0);
 }
