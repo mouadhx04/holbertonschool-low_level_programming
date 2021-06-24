c9b2aa1101a6a8c7678fd606fcd1085de1328b4c
mouadhx04 <mouadh1500@gmail.com>
2021-06-24 06:02:44 -0700

    print digits 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
+
 /**
  * more_numbers - prints 10 times the numbers, from 0 to 14
  *
  *
- * Return: 0 (success)
+ * 
  */
 void more_numbers(void)
 {
-int i;
-int j;
+int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
