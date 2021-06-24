2687fdb694cf4bc13189dd0eb79a3b2e98d4123f
mouadhx04 <mouadh1500@gmail.com>
2021-06-24 04:17:21 -0700

    my library

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,3 +1,7 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -9,3 +13,4 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 int _putchar(char c);
+#endif /* HOLBERTON_H */
