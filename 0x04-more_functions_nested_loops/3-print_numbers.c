5738bd8283d4ee46a73d03804e4cc821048adfe4
mouadhx04 <mouadh1500@gmail.com>
2021-06-24 03:23:07 -0700

    print all digits

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ *
+ *
+ * Return: 0 (success)
+ */
+void print_numbers(void)
+{
+	if (c > '0' && c < '9')
+	{
+		_putchar(c + '0');
+	}
+	_putchar('\n');
+	return (0);
+}
