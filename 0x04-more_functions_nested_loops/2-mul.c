ec9a9fb8867db3123344902de4974e5c17ce21f8
mouadhx04 <mouadh1500@gmail.com>
2021-06-24 03:16:42 -0700

    multiplies two integer

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * mul - multiplies two integers
+ *@a: The character to multiplie
+ *@b: the character to multilpie
+ * Return: 0 (success)
+ */
+int mul(int a, int b)
+{
+	int x;
+	if (a != 0 && b != 0)
+	{
+		x = a * b;
+		_putchar(x + '0');
+	}
+	return (0);
+}
