16cb31c2390df8b9ddbce1748b78096817bb4413
mouadhx04 <mouadh1500@gmail.com>
2021-06-24 05:12:27 -0700

    print all digits

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 /**
  * print_numbers - prints the numbers, from 0 to 9
- *
+ *@c: character to print
  *
  * Return: 0 (success)
  */
 void print_numbers(void)
 {
-	for (c >= '0'; c <= '9'; c++)
+	for (c >= 48; c <= 57; c++)
 	{
-		_putchar(c + '0');
+		_putchar(c);
 	}
 	_putchar('\n');
 	return (0);
