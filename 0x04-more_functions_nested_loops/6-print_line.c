b7afefbeaae5e913adbf19f1ded815eaf1bc7127
mouadhx04 <mouadh1500@gmail.com>
2021-06-24 13:03:52 -0700

    print a straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,12 +1,18 @@
 #include "holberton.h"
 /**
  * print_line - draws a straight line in the terminal
+ * @n: number of character
  */
 void print_line(int n)
 {
+int i;
+
 	if (n <= 0)
 		_putchar('\n');
 	else
-		_putchar('-');
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('-');
+		}
+	_putchar('\n');
 }
-
