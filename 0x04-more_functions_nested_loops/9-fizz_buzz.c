0c83b82c411e4ee509e91d176b400b2250f8846f
mouadhx04 <mouadh1500@gmail.com>
2021-06-24 15:49:23 -0700

    fizz buzz game

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,14 +9,18 @@ int i;
 
 for (i = 1; i <= 100; i++)
 {
-if (((i % 3) || (i % 5)) == 0)
+if ((i % 3) == 0 && (i % 5) == 0)
 printf("FizzBuzz");
+
 else if ((i % 3) == 0)
 printf("Fizz");
+
 else if ((i % 5) == 0)
 printf("Buzz");
+
 else
 printf("%d", i);
+
 if (num == 100)
 continue;
 printf(" ");
