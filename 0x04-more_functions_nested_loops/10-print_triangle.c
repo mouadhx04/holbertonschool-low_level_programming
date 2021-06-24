3b76b9db980a6f9010fa475c450e8448c38fbb9b
mouadhx04 <mouadh1500@gmail.com>
2021-06-24 16:00:11 -0700

    draw a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -5,3 +5,24 @@
  */
 void print_triangle(int size)
 {
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = size - i; j > 0; j--)
+				_putchar(' ');
+
+			for (j = 0; j < i; j++)
+				_putchar('#');
+
+			if (i == size)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}
