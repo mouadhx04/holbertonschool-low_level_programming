c237d91603da8cd529c046801449689383b872e2
mouadhx04 <mouadh1500@gmail.com>
2021-06-24 05:32:41 -0700

    print not 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,13 +7,14 @@
  */
 void print_most_numbers(void)
 {
-	for (i >= '0'; i <= '9'; i++)
+int i;
+
+	for (i = 0; i <= 9; i++)
 	{
 		if (i != 2 && i != 4)
 		{
-			_putchar(i + '0');
+			_putchar((i % 10) + '0');
 		}
 	}
 	_putchar('\n');
-	return (0);
 }
