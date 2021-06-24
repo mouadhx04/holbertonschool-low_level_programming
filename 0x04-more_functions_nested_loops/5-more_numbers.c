964edc7fbaebc092dc3814bbd7bca76e5dfcc4ae
mouadhx04 <mouadh1500@gmail.com>
2021-06-24 05:42:30 -0700

    print digits 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,12 +8,13 @@
 void more_numbers(void)
 {
 int i;
+int j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = '0'; i <= '10'; i++)
 	{
-		for (i = '0'; i <= '14'; i++)
+		for (j = '0'; j <= '14'; j++)
 		{
-			_putchar((i % 10) + '0');
+			_putchar(j + '0');
 		}
 	}
 	_putchar('\n');
