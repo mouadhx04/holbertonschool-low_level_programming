914bbe522dfb038249e59363e1c20552e6740a39
mouadhx04 <mouadh1500@gmail.com>
2021-06-24 03:36:49 -0700

    print digits without 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * print_most_numbers - prints the numbers, from 0 to 9 and Do not print 2 and 4
+ * print_most_numbers - prints the digits numbers, not print 2 and 4
  *
  *
  * Return: 0 (success)
