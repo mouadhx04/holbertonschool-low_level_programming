e98b3221c8936c2ad657170db38934e0abfddbe0
mouadhx04 <mouadh1500@gmail.com>
2021-06-24 14:33:35 -0700

    prints diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,21 +1,27 @@
 #include "holberton.h"
 
 /**
- * print_diagonal - Draws a draws a diagonal line using the character \.
- * @n: The number of \ .
+ * print_diagonal - Draws a diagonal line with \.
+ * @n: The number of \ charactes
  */
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
-			_putchar(' ');
-			if (i == n)
-			{
-				_putchar('\');
-			}
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+
+			if (i == n - 1)
+				continue;
+
+			_putchar('\n');
+		}
 	}
+
 	_putchar('\n');
 }
