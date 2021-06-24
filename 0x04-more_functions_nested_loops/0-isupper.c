f6d0be1dc425bd36caff8ad02dedaadde070fc6c
mouadhx04 <mouadh1500@gmail.com>
2021-06-24 01:42:11 -0700

    check uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _isupper - checks for uppercase character
+ *@c: The character to check
+ *
+ * Return: 1 (success), 0 (otherwize)
+ */
+int _isupper(int c)
+{
+	if (c > 'A' && c < 'Z')
+	{
+		_putchar(c);
+		return (1);
+	}
+	else
+	{
+		_putchar(c);
+		return (0);
+	}
+}
