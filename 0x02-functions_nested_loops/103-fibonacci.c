0954c468a82ffe7eff882330c5db5368f689cea8
mouadhx04 <mouadh1500@gmail.com>
2021-06-26 11:08:18 -0700

    sum even-valued fibonnacci terms

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - prints sum even terms for first 32 Fibonacci
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = 0;
+	int y = 1;
+	int i;
+	int s = x + y;
+	int sum;
+
+	for (i = 3; i < 32; i++)
+	{
+		x = y;
+		y = s;
+		s = x + y;
+		if ((s % 10) == 0)
+			sum = sum + s;
+	}
+	printf("%d", sum);
+	printf("\n");
+	return (0);
+}
