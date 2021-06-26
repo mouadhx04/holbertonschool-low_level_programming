f3613b64ba75cee05f1c7335f81d265da4f9d18d
mouadhx04 <mouadh1500@gmail.com>
2021-06-26 08:47:16 -0700

    print sum multiples of 3 and 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,28 +1,20 @@
 #include <stdio.h>
 /**
  *main - prints sum of multiples of 3 and 5
- *@i: counter
- *@x,y: variable for sum
- *@s: sum x,y
+ *@i: varaiable counter
+ *@x: variable for sum multiples
  *return: always 0 (success)
  */
 int main(void)
 {
-	int i, x, y, s;
+	int i, x;
 
 	for (i = 0; i < 1024; i++)
 	{
-		if ((i % 3) == 0)
-		{
+		if (((i % 3) == 0) || ((i % 5) == 0))
 			x == x + i;
-		}
-		else if ((i % 5) == 0)
-		{
-			y == y + i;
-		}
-		s == x + y;
 	}
-	printf("somme multiples of 3 and 5 is %d\n", s);
+	printf("somme multiples of 3 and 5 is %d\n", x);
 	printf("\n");
 	return (0);
 }
