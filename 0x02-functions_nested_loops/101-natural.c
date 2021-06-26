e17a17f28c0c4ec78a15a316587a62cb04a95607
mouadhx04 <mouadh1500@gmail.com>
2021-06-26 08:07:28 -0700

    print sum multiples of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * print_times_table - prints the n times table, starting with 0.
+ *@i: counter
+ *@x,y: variable print somme multiplies 3,5.
+ *@s: cum x,y
+ *return: 0 (success)
+ */
+int main (void)
+{
+	int x, y, s;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 = 0)
+			x == x + i;
+		else if (i % 5 = 0)
+			y == y + i;
+	}
+	s == x + y;
+	printf("somme multiples of 3 is %d\n", x);
+	printf("somme multiples of 5 is %d\n", y);
+	printf("somme multiples of 3 and 5 is %d\n", s);
+	return (0);
+}
