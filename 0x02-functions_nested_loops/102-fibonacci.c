af169907725cda85320fb5c7adae4644c02a7289
mouadhx04 <mouadh1500@gmail.com>
2021-06-26 09:45:09 -0700

    print fibonacci number

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints sum of multiples of 3 and 5
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = 0;
+	int y = 1;
+	int s;
+
+	while (s < 50)
+		s = x + y;
+	printf("%d", s);
+	printf(",");
+	printf(" ");
+	return (0);
+}
