ad0e9d1483ceb765a6e6cc4fc9ace64b7bf50dc6
mouadhx04 <mouadh1500@gmail.com>
2021-06-26 07:25:18 -0700

    function to print n times

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,7 @@
+#include "holberton.h"
+/**
+ * print_times_table - prints the n times table, starting with 0.
+ *@n: variable for nuber of repetition the print
+ *
+ */
+void print_times_table(int n)
