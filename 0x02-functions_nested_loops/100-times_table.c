a64348374dea410ca1baa5bf46c4b0124692c0b9
mouadhx04 <mouadh1500@gmail.com>
2021-06-26 07:31:30 -0700

    function to print n times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,3 +5,40 @@
  *
  */
 void print_times_table(int n)
+{
+	{
+	int i, j, x;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('0');
+
+			for (j = 1; j <= n; j++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				x = i * j;
+
+				if (x <= 99)
+					_putchar(' ');
+				if (x <= 9)
+					_putchar(' ');
+
+				if (x >= 100)
+				{
+					_putchar((x / 100) + '0');
+					_putchar(((x / 10)) % 10 + '0');
+				}
+				else if (x <= 99 && x >= 10)
+				{
+					_putchar((x / 10) + '0');
+				}
+				_putchar((x % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
