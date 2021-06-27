e978d7d1022b572aca78c8e1b4d8e877d69d4be1
mouadhx04 <mouadh1500@gmail.com>
2021-06-27 10:47:12 -0700

    print with div and mod

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,7 +7,16 @@
  */
 void print_number(int n)
 {
-	_putchar((n / 10) + '0');
-	_putchar((n % 10) + '0');
-}
+	unsigned int n;
 
+	if (n < 0)
+	{
+		putchar('-');
+		n = -n;
+	}
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+		_putchar((n % 10) + '0');
+	}
+}
