67031deadd6b55c4cd53a125bf18e00b465db81a
mouadhx04 <mouadh1500@gmail.com>
2021-06-27 10:55:09 -0700

    print with div and mod

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,22 +1,19 @@
 #include "holberton.h"
 /**
- * print_number - prints an integer
- *
- *@n: integer to be printed
- *
+ * print_number - Prints an integer
+ * @n: The integer to be printed
  */
+
 void print_number(int n)
 {
-	unsigned int n;
+	unsigned int x = n;
 
 	if (n < 0)
 	{
-		putchar('-');
-		n = -n;
+		_putchar('-');
+		x = -x;
 	}
-	if ((n / 10) != 0)
-	{
-		print_number(n / 10);
-		_putchar((n % 10) + '0');
-	}
+	if ((x / 10) > 0)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
 }
