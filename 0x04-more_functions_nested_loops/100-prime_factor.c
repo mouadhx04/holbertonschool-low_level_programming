4c2401801265d4eb038f1eea02c0b8ba1fb52e61
mouadhx04 <mouadh1500@gmail.com>
2021-06-27 08:53:22 -0700

    print prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,13 +6,25 @@
  */
 int main(void)
 {
-	int i, n;
+	long int n;
+	n = 612852475143;
+	long int div = 2, ans = 0, maxFact;
 
-	for (i = 0; i < (n / 2); i++)
+	while(n!=0)
 	{
-		if ((n % i) == 0)
-			printf("%d", i);
+		if(n % div !=0)
+			div = div + 1;
+		else
+		{
+			maxFact = n;
+			n = n / div;
+			if(n == 1)
+			{
+				printf("%d", maxFact);
+				ans = 1;
+				break;
+			}
+		}
 	}
-	printf("\n");
 	return (0);
 }
