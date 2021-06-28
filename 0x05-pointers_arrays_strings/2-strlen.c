ba7e6fb83dd284ac07fb884c04cd2132d21cdf77
mouadhx04 <mouadh1500@gmail.com>
2021-06-28 04:38:07 -0700

    length of a pointer sting

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
 /**
- * _strlen - returns the length of a string
- *@s: is a pointer to char 
- *
+ *_strlen - returns the length of a string
+ *@s: is a pointer to char
+ *Return: len (Success)
  */
 int _strlen(char *s)
 {
 	int len;
-
 	size_t len = strlen(s);
+
+	return (len);
 }
