d675643a8752c89fd30475ba9dc49ef7ba3b779b
mouadhx04 <mouadh1500@gmail.com>
2021-06-28 05:34:15 -0700

    create puts pointer string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *_puts - prints a string
+ *@str: is a pointer to char
+ *Return: 0 (Success)
+ */
+void _puts(char *str)
+{
+	int c;
+
+	while (*str++)
+		_putchar(str);
+	return (0);
+}
