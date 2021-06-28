5eb3affeae33abc741492cb432cd59d7b0fbf6a1
mouadhx04 <mouadh1500@gmail.com>
2021-06-28 15:08:02 -0700

    putchar second half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+int _strlen(char *s);
+/**
+ *puts_half - prints second half of a string
+ *@str: is a pointer to char and the parameter
+ *
+ */
+void puts_half(char *str)
+{
+	int i;
+	int x = _strlen(str);
+
+	for (i = (i / 2); i < x; i++)
+		_putchar(str[i]);
+	_putchar ('\n');
+}
+
+#include "holberton.h"
+/**
+ *_strlen - returns the length of a string
+ *@s: is a pointer to char
+ *Return: len (Success)
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+		len++;
+	return (len);
+}
