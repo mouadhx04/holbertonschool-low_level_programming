91e95c494cad8a4cb227223bc47ba9ff8153b8b5
mouadhx04 <mouadh1500@gmail.com>
2021-06-28 14:42:42 -0700

    _putchar 0 + 2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,8 +1,30 @@
 #include "holberton.h"
+int _strlen(char *s)
 /**
  *puts2 - prints every other character of a string
  *@s: is a pointer to char
  *
  */
 {
+	int i;
+	int x = _strlen(str);
 
+	for (i = 0; i < x; i += 2)
+		_putchar(str[i]);
+	_putchar ('\n');
+}
+
+#include "holberton.h"
+/**
+ *_strlen - returns the length of a string
+ *@s: is a pointer to char
+ *Return: len (Success)
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+		len++;
+	return (len);
+}
