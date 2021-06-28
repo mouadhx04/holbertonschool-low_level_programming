9e0de55b2f0d43e81891449b7cb00d70b620782a
mouadhx04 <mouadh1500@gmail.com>
2021-06-28 05:41:14 -0700

    create puts pointer string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,11 +2,11 @@
 /**
  *_puts - prints a string
  *@str: is a pointer to char
- *Return: 0 (Success)
+ *
  */
 void _puts(char *str)
 {
-	while (*str++)
-		_putchar(str);
-	return (0);
+	while (*str)
+		_putchar(*str++);
+	_putchar ('\n');
 }
