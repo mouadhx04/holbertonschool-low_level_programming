ec3ed26506f26203ded1cdd524e9f173b27cc26a
mouadhx04 <mouadh1500@gmail.com>
2021-06-28 13:34:22 -0700

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,4 +6,16 @@
  */
 void rev_string(char *s)
 {
+	int len = 0, i = 0;
+	char x;
 
+	while (s[i++])
+		len++;
+
+	for (i = len - 1; i >= len / 2; i--)
+	{
+		x = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = x;
+	}
+}
