427e33a5c04ec8545e1b1a4b66967cb88d4e1b96
mouadhx04 <mouadh1500@gmail.com>
2021-06-28 06:03:30 -0700

    pritn in string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+int _strlen(char *s);
+/**
+ *print_rev - prints a string, in reverse
+ *@s: is a pointer to char
+ *
+ */
+void print_rev(char *s)
+{
+	int c, len = _strlen(s);
+
+	for (c = len -1; c >= 0; c--)
+		_putchar(s[c]);
+	_putchar('\n');
+}
+
+/**
+ *_strlen - returns the length of a string
+ *@s: is a pointer to char
+ *Return: len (Success)
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+		len++;
+	return (len);
+}
