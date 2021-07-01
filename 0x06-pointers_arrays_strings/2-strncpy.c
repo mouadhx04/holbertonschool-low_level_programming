79e914acd59a4024a3fb848d007d56fed1245003
mouadhx04 <mouadh1500@gmail.com>
2021-07-01 13:34:21 -0700

    copy n from src to destination

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *_strncpy - copies a string
+ *@dest: char pointer
+ *@src: char pointer
+ *@n: number of bytes from src
+ *Return: dest (Success)
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	while (*source && num--)
+	{
+		*destination = *source;
+		destination++;
+		source++;
+	}
+	*destination = '\0';
+}
+
