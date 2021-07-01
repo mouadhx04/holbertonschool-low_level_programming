6c2c745d6dfd81c8142d8e1fe4b75af091b47939
mouadhx04 <mouadh1500@gmail.com>
2021-07-01 15:56:01 -0700

    encodes a string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,3 +6,19 @@
  * Return: char
  */
 char *leet(char *str)
+{
+	int i, j;
+	char num[] = {'4', '3', '0', '7', '1'};
+	char min[] = {'a', 'e', 'o', 't', 'l'};
+	char maj[] = {'A', 'E', 'O', 'T', 'L'};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if ((str[i] == min[j]) || (str[i] == maj[j]))
+			str[i] = num[j];
+		}
+	}
+	return (str);
+}
