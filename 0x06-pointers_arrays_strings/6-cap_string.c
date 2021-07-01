6573d070be3bd2683b08c7d9ec4e4d6bed32a277
mouadhx04 <mouadh1500@gmail.com>
2021-07-01 15:45:21 -0700

    capitalizes all word in string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,4 +5,17 @@
  *Return:str;
  */
 char *cap_string(char *str)
+{
+	int i, j;
+	char ch[] = {',', '\t', '\n', ' ', ';', '.',
+				 '!', '?', '"', '(', ')', '{', '}'};
 
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (i == 0)
+			{
+				str[0] = str[0] - 32;
+			}
