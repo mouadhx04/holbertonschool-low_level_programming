6370a256f4f1f1f1233fdfee50d05edd1283dc4c
mouadhx04 <mouadh1500@gmail.com>
2021-07-01 02:07:57 -0700

    concatenate two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 /**
- *_strlen - returns the length of a string
- *@s: is a pointer to char
- *Return: len (Success)
+ *_strncat - concatenates two strings
+ *@dest: char pointer
+ *@src: char pointer 
+ *
  */
 char *_strcat(char *dest, char *src)
 {
