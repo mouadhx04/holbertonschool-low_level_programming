597b25d632fd8082b5071ab1e65c49339a275f75
mouadhx04 <mouadh1500@gmail.com>
2021-07-01 13:55:15 -0700

    copy n from src to dest

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,12 +8,8 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	while (*src && n--)
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
-	*dest = '\0';
-}
+	int i;
 
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+}
