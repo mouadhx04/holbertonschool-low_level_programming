838af5e9c7242733b61b2ed093e6ca40075601f3
mouadhx04 <mouadh1500@gmail.com>
2021-07-03 13:51:51 -0700

    crypte with rot13 like leet method

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,12 +9,13 @@ char *rot13(char *str)
 {
 	int i, j;
 
-	char alpha[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-	char rot13x[] = {'n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M'};
+	char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13x[] =   "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 	for (i = 0; str[i] != '\0'; i++)
 		for (j = 0; j < 52; j++)
 			if (str[i] == alpha[j])
 				str[i] = rot13x[j];
-	return (str);
+	j = 52;
+	return (s);
 }
