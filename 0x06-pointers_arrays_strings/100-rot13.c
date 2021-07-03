deb1cf31487689f286fbc68bb8d8eccd676d028c
mouadhx04 <mouadh1500@gmail.com>
2021-07-03 06:06:01 -0700

    encodes string to rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *rot13 - encodes a string using rot13
+ * @str: string used
+ *
+ * Return: char
+ */
+char *rot13(char *str)
+{
+	int i, j;
+
+	if (str[i] != '\0')
+	{
+		for (i >= 97; i <= 122; i++)
+		{
+			for (j >= 65; j <= 90; j++)
+				str[i] = str[i] + 13;
+				str[j] = str[j] + 13;
+		}
+	}
+return (str);
+}
+
