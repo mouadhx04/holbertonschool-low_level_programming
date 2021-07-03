23f5cc5871ea5b9c1a5ab71a2c3aa4ab1e751580
mouadhx04 <mouadh1500@gmail.com>
2021-07-03 06:52:20 -0700

    encodes string to rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,14 +8,15 @@
 char *rot13(char *str)
 {
 	int i, j;
+	int x = 13;
 
 	if (str[i] != '\0')
 	{
 		for (i >= 97; i <= 122; i++)
 		{
 			for (j >= 65; j <= 90; j++)
-				str[i] += 13;
-				str[j] += 13;
+				str[i] = str[i] + x;
+				str[j] = str[j] + x;
 		}
 	}
 return (str);
