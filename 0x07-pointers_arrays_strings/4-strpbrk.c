64ee16a2453319747d9e9a105c8a3b0ca999ca10
mouadhx04 <mouadh1500@gmail.com>
2021-07-05 15:25:18 -0700

    locates the first occurrence in the string

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,16 +10,15 @@
 */
 char *_strpbrk(char *s, char *accept)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (i = 0; accept[i] != '\0'; i++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (s[i] == accept[i])
+			if (s[i] == accept[j])
 			return (s + i);
 		}
-		i++;
 	}
 
 		return ('\0');
