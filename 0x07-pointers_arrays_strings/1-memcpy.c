0816cb81f65bc465ccaf59d9d78c28cdea763af2
mouadhx04 <mouadh1500@gmail.com>
2021-07-05 04:59:57 -0700

    fill memory dest from memory src

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+* _memcpy - function copies n bytes from memory area src to memory area dest
+* Returns a pointer to dest
+* @dest: A pointer to the memory area to be filled.
+* @src: A pointer to copy or fill the memory area dest
+* @n: The number of bytes to be filled or copied
+*
+* Return: A pointer to the filled memory area @s.
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
