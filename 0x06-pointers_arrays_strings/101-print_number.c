dbb8cfe41305a638ff2026bc5ff98ac9abfbaa18
mouadhx04 <mouadh1500@gmail.com>
2021-07-06 10:10:30 -0700

    print number with putchar

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,14 +6,15 @@
 
 void print_number(int n)
 {
-	unsigned int x = n;
-
 	if (n < 0)
 	{
 		_putchar('-');
-		x = -x;
+		n = -n;
 	}
-	if ((x / 10) > 10)
-		print_number(x / 10);
-	_putchar((x % 10) + '0');
+	else if (n == 0)
+		_putchar('0');
+
+	else ((n / 10) > 0)
+		print_number(n / 10);
+	_putchar((n % 10) + '0');
 }
