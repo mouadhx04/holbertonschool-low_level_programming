97845af527c3023f791254d9f9580af94447b103
mouadhx04 <mouadh1500@gmail.com>
2021-07-06 09:31:29 -0700

    print number with putchar

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -13,7 +13,7 @@ void print_number(int n)
 	}
 	if (n == 0)
 		_putchar('0');
-	if (n/10)
-		print_number(n/10);
-	_putchar(n%10 + '0');
+	if (n / 10)
+		print_number(n / 10);
+	_putchar((n % 10) + '0');
 }
