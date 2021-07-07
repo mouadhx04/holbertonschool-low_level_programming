2fb05e642a39736049ec282e32862e3e54111724
mouadhx04 <mouadh1500@gmail.com>
2021-07-07 10:47:00 -0700

    print x power y with recursive

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -17,10 +17,6 @@ int _pow_recursion(int x, int y)
 	{
 		return (1);
 	}
-	else if (x < 0)
-	{
-		return (_pow_recursion(x * -1, y - 1));
-	}
 	else
-		return (_pow_recursion(x, y - 1));
+		return (x * _pow_recursion(x, y - 1));
 }
