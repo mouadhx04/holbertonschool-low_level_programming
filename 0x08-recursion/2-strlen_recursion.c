12e4f173dfb65176e0f7617289dd48d4a40ed8cd
mouadhx04 <mouadh1500@gmail.com>
2021-07-07 07:34:58 -0700

    print string length with recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* _strlen_recursion - returns the length of a string
+*
+* @s: string to bescanned for get the length
+*Return: integer or 0 (Success)
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		x++;
+	return (x + (_strlen_recursion(s + 1)));
+	}
+}
