1566402f1f6ad6f3bad45cc260d88547cc875326
mouadhx04 <mouadh1500@gmail.com>
2021-07-07 09:58:28 -0700

    print x power y with recursive

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+* _pow_recursion - returns the value of x raised to the power of y.
+*
+* @x: integer to be printed with power y
+* @y: integer power
+*Return: integer or 0 (Success)
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 1)
+	{
+		return (x * y);
+	}
+	else
+		return (factorial(x * (y- 1)));
+}
