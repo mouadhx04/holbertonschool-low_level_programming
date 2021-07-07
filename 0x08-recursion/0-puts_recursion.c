20cb1d8213d5c6bc2284e87436c1cad975228b7e
mouadhx04 <mouadh1500@gmail.com>
2021-07-07 04:34:03 -0700

    print with recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* _puts_recursion - prints a string
+*
+* @s: string to be printed
+* 
+*/
+void _puts_recursion(char *s)
+{
+	int i;
+
+	if (s[i] != '\0')
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\0');
+}
