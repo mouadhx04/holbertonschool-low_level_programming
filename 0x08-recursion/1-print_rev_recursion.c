a1346818204319c228e542dce7ff804d4235cc9f
mouadhx04 <mouadh1500@gmail.com>
2021-07-07 13:21:12 -0700

    print a string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,13 +8,9 @@
 */
 void _print_rev_recursion(char *s)
 {
-	if (*s == 0)
+	if (*s)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-	_putchar(*s);
-	_print_rev_recursion(s - 1);
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 }
