fe22133e718d69a63d76fbbfa633188d32e626da
mouadhx04 <mouadh1500@gmail.com>
2021-07-07 12:46:57 -0700

    check number is prime or not

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _sqrt_recursion - calculates the natural square root of a number
+ * @nb: the given number
+ *
+ * Return: 1 or 0 (Success)
+ */
+int is_prime_number(int n)
+{
+	if ((n % 2) = 0)
+		return (0);
+	else
+		return (1);
+}
