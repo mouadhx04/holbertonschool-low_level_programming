bc51a81779a446f733339069fd1bb9fdc93323bb
mouadhx04 <mouadh1500@gmail.com>
2021-07-07 06:04:32 -0700

    print a string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* _print_rev_recursion - prints a string in reverse
+*
+* @s: string to be printed in reverse
+*
+*/
+void _puts_recursion(char *s)
+{
+	*s--;
+	if (*s == 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+	}
+}
