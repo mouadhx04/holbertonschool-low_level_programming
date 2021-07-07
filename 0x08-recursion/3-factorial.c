e7a3b24fda18eca64a8206db3757e941b32c32f2
mouadhx04 <mouadh1500@gmail.com>
2021-07-07 07:55:56 -0700

    get a factorial recursively

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* factorial - returns the factorial of a given number
+*
+* @n: string to bescanned for getting his factorial
+*Return: integer or 0 (Success)
+*/
+int factorial(int n)
+{
+	if (n == '0')
+	{
+		return (-1);
+	}
+	else
+	return (n + _strlen_recursion(s + 1));
+}
