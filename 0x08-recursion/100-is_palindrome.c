a40aed9348c42e6fad4122bbeba98cacb60df933
mouadhx04 <mouadh1500@gmail.com>
2021-07-07 15:06:25 -0700

    palindrome checking

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+int _strlen(char *s);
+/**
+ *is_palindrome - returns 1 if a string is a palindrome and 0 if not.
+ *@s: variable to be checked
+ *Return: 1 or 0 (Success)
+ */
+int is_palindrome(char *s)
+{
+	if (*s++  == s[len--])
+		return (1);
+	else
+		return (0);
+}
+/**
+ *_strlen - returns the length of a string
+ *@s: is a pointer to char
+ *Return: len (Success)
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+		len++;
+	return (len);
+}
