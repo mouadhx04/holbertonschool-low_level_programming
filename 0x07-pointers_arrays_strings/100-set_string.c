36158ce7c5ffe2f0d01156463d60139e9651935f
mouadhx04 <mouadh1500@gmail.com>
2021-07-08 10:13:39 -0700

    set a value of a pointer to char in one line

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,30 +1,11 @@
 #include "holberton.h"
-int _strlen(char *s);
+
 /**
- *set_string - sets the value of a pointer to a char
- *@s: is a poiner to pointer type char
- *@to: is a pointer to char
+ * set_string - Sets the value of a pointer to a char
+ * @s: The pointer 
+ * @to: The char
  */
 void set_string(char **s, char *to)
 {
-	int i, j, len;
-	char x;
-
-	len = _strlen(**s);
-	for (i = 0; i <= len; i++)
-	{
-		for (j = 0; j <= len; j++)
-			x = s[i];
-		to[j] = x;
-	}
-	to[j] = '\0';
+	*s = to;
 }
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s++)
-		len++;
-	return (len);
-}
-
