e05fd6333b850efc4113345721ef97159506c088
mouadhx04 <mouadh1500@gmail.com>
2021-07-09 06:10:39 -0700

    count the path

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print the name of the program
+ * @argc: The number of command line arguments in argv array
+ * @argv: An array containing the program command line arguments
+ * Return: 0 (Success)
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
