a3753b1377cec22f62f0ced90515939e3edf9201
mouadhx04 <mouadh1500@gmail.com>
2021-07-09 05:34:14 -0700

    give me the program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
  * @argv: An array containing the program command line arguments
  * Return: integer or 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
 	printf("Name of the program is %s\n", argv[0]);
 	return (0);
