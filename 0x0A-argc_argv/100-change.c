988882dd1fbef4af643d0093f9dd091f5c1a1e12
mouadhx04 <mouadh1500@gmail.com>
2021-07-10 10:23:22 -0700

    print minimum number of coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,31 +10,43 @@
  */
 int main(int argc, char *argv[])
 {
-	int cents;
+	int cents, coins = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else if (atoi(argv[1]) < 0)
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
 	{
-		printf("0\n");
+		coins++;
+		if ((cents - 25) >= 0)
+		{
+			cents -= 25;
+			continue;
+		}
+		if ((cents - 10) >= 0)
+		{
+			cents -= 10;
+			continue;
+		}
+		if ((cents - 5) >= 0)
+		{
+			cents -= 5;
+			continue;
+		}
+		if ((cents - 2) >= 0)
+		{
+			cents -= 2;
+			continue;
+		}
+		cents--;
 	}
-	else if (atoi(argv[1]) >= 25)
-	{
-		cents = (atoi(argv[1]) / 25) + (atoi(argv[1]) % 25);
-		printf("%d\n", cents);
-	}
-	else if (atoi(argv[1]) == 10)
-	{
-		cents = (atoi(argv[1]) / 10);
-		printf("%d\n", cents);
-	}
-	else if (atoi(argv[1]) > 10)
-	{
-		cents = (atoi(argv[1]) % 10);
-		printf("%d\n", cents);
-	}
+
+	printf("%d\n", coins);
+
 	return (0);
 }
