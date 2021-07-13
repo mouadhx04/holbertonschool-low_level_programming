ce347eb6132fe02b0427bd16913a464e0fd754b6
mouadhx04 <mouadh1500@gmail.com>
2021-07-13 11:22:59 -0700

    give me a location in the memory

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,13 +11,19 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *p = &c;
+	unsigned int i;
+	char *p;
 
 	if (size == 0)
 		return (0);
 
 	p = (char *)malloc(size * sizeof(char));
-
-	printf("%s", p);
+	if (p == NULL)
+		return (0);
+	else
+	{
+		for (i = 0; i <= size; i++)
+			p[i] = c;
+	}
 	return (p);
 }
