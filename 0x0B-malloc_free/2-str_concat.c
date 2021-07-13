9e626e6c2d2bdb079ab114f6ac4b86f8aa2a216c
mouadhx04 <mouadh1500@gmail.com>
2021-07-13 15:29:12 -0700

    concatenate with malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,13 +13,16 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, len;
 	char *k;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
-		return (NULL);
+		s1 = "";
 	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	len = strlen(s1) + strlen(s2);
 
-	k = malloc(len * sizeof(char));
+	k = malloc((len + 1) * sizeof(char));
 	if (k == NULL)
 	{
 		return (NULL);
