c5120c8cbf7b782397f00ca1087b509a1027f56b
mouadhx04 <mouadh1500@gmail.com>
2021-07-15 05:19:31 -0700

    allocate memory with differet size

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: size of memory allocation
+ *
+ * Return: pointer or 0 (Success)
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit (98);
+	}
+	return (p);
+}
