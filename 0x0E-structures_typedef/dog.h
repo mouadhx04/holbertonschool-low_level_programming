eecb34eabfefb130b6211260643f02f84fe67a8c
mouadhx04 <mouadh1500@gmail.com>
2021-07-19 05:17:40 -0700

    description of a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - description for a dog
+ * @name: name of a dog
+ * @age: age of a dog
+ * @owner: name owner of the dog
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
