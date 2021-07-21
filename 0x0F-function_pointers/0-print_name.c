8fb161e5245718a334f48427977a7f04345653fc
mouadhx04 <mouadh1500@gmail.com>
2021-07-21 10:11:35 -0700

    don't need to create func printf before

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name.
+ * @name: char
+ * @f: pointer to a function
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+		return;
+	f(name);
+}
