d2a1592bce0579656039f5d5f89c2a2469e1f330
mouadhx04 <mouadh1500@gmail.com>
2021-07-21 12:30:20 -0700

    focus to the question details

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - returns the index of the first element for which
+ * the cmp function * does not return 0
+ * @array: pointer to an array
+ * @size: array size
+ * @cmp: pointer to function that compare values
+ * Return: nothing
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (cmp == NULL || size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
