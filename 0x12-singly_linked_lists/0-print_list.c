f2053e8963af61e354789c31bf7be73394e79a58
mouadhx04 <mouadh1500@gmail.com>
2021-07-29 09:27:59 -0700

    just focus and learn from anywhere

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,7 +8,7 @@
 **/
 size_t print_list(const list_t *h)
 {
-	unsigned int i = 0;
+	size_t len;
 
 	while (h != NULL)
 	{
@@ -18,7 +18,7 @@ size_t print_list(const list_t *h)
 			printf("[%u] %s\n", h->len, h->str);
 
 		h = h->next;
-		i++;
+		len++;
 	}
-	return (i);
+	return (len);
 }
