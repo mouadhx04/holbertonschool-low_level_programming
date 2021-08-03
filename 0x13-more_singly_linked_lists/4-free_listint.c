58ccb6d36d737adb7c045a36bffd08ba3d257246
mouadhx04 <mouadh1500@gmail.com>
2021-08-03 12:28:56 -0700

    free all the allocation used

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* free_listint - function that frees a listint_t list.
+* @head: listint_t pointer to a list node
+*/
+void free_listint(listint_t *head)
+{
+	listint_t *aux;
+
+	while (head != NULL)
+	{
+		aux = head;
+		head = head->next;
+		free(aux);
+	}
+}
