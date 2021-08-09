98be025fc77f70547284fdf8856c0fc4419a6d83
mouadhx04 <mouadh1500@gmail.com>
2021-08-09 07:06:58 -0700

    creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, rw, len = 0;
-	
+
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
