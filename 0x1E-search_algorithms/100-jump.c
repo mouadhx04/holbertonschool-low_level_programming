3317b71bcfcb2925e9367a4c286e72f54ca03178
root <root@Mouadh.localdomain>
2022-01-19 02:04:03 +0100

    Jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * jump_search - searche for a value in an array of integers
+ * jump_search - searche for a value in an array of integers.
  *
  * @array: a pointer to the first element of the array to search in
  * @size: the number of elements in array
